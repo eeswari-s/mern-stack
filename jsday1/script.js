fddcaae4f1770816b97a1626c4f42c92404f3dba
eeswari-s <eeswarivaithee@gmail.com>
2025-10-07 16:30:33 +0530

    com

---
+++ jsday1/script.js
@@ -0,0 +1,39 @@
+//variables:
+
+//var
+//let
+//const
+
+
+
+
+//     var mernstack ="students";
+//  console.log(mernstack);//
+// var a = 10;
+//   a =20;//redeclare,reasign
+// console.log(a);
+
+// let a =10;
+//  a =20;
+// console.log(a);
+
+// const a =20;
+// console.log(a);
+// var b=30;
+// let c=40;
+// const d=50;
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// const city="pune";
+
+// console.log(city);
+
+var shami = "voice";
+
+console.log( shami);
+
+
+//word---string
+//10------number
+//mernstack---string
