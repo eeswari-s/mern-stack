3e8afc28df6a2a0b335bec6d3cc1ebde08fae601
eeswari-s <eeswarivaithee@gmail.com>
2025-10-08 11:15:54 +0530

    com

---
+++ Day20/Js2/script.js
@@ -0,0 +1,63 @@
+// let a;
+// console.log(a);
+
+// let acc=null;
+// console.log(acc);
+// console.log(typeof acc);
+
+// let bigNymber=123456789012345678901234567890123456789012345678901234567890;
+// console.log(bigNymber);
+// console.log(typeof bigNymber);
+
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+// console.log(id1===id2);
+
+// //5=="5"
+// //5==="5"
+
+// let isLoggedin = true;
+// let isADmin = false;
+// console.log(isLoggedin && !isADmin);
+
+
+// console.log("BION DATA");
+
+// let fname = "Vaitheeswari";
+// console.log("Name: " + fname);
+
+// let lname = "Sivanantham";
+// console.log("last name: " + lname);
+
+// 
+
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(++a);
+// console.log(--a);
+
+
+let x =5;
+x *=5;
+ x /=5;
+x %=5;
+ x **=5;
+       
+console.log(x);
+
+
+
+console.log(5 =="5");
+console.log(5 ==="5");
+console.log(50 >15);
+console.log(5 <"5");
+console.log(5 >= "5");
+console.log(5 >= 5);
+console.log(5 <= "5");
+console.log(5 != 6);
