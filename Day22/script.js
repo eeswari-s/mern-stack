979bbae86cd7bdbb762b0efd0a19e3f8169442be
eeswari-s <eeswarivaithee@gmail.com>
2025-10-10 10:35:19 +0530

    comit

---
+++ Day22/script.js
@@ -0,0 +1,55 @@
+// function hello(){
+//     console.log("hello world");
+// }
+//  hello();
+
+
+//  function mernstack(name)
+//  {
+//      console.log("My name is: "+name);
+//  }
+//  mernstack("rubini");
+
+
+
+//  let fname="rubini";
+//  console.log("My name is: "+fname);
+
+
+let marks=70;
+  
+if(marks >=90)
+{
+    console.log("A grade");
+}
+else if(marks >=80)
+{
+    console.log("B grade");
+}
+else if(marks >=70)
+{
+    console.log("C grade");
+}
+
+else
+    {
+    console.log("Fail");
+}
+
+
+// let i=1;
+// while(i < 30)
+// {
+//     console.log("Number: "+i); 
+//     i++;
+// }
+
+let i=1;
+do 
+{
+    console.log("Numbe: "+i);
+i++; 
+}
+while(i <= 30);
+
+
