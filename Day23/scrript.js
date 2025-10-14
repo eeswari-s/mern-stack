098981892a181671bff8c8c59bb7d30ca637f472
eeswari-s <eeswarivaithee@gmail.com>
2025-10-14 22:43:20 +0530

    s

---
+++ Day23/scrript.js
@@ -0,0 +1,87 @@
+// let course = "taMiL";
+ 
+// switch (course)
+// {
+// case "java":
+//     console.log("java is a programming language");
+//     break;
+// case "python":
+//     console.log("python is a programming language");
+//     break;
+//     case "mernstack":
+//     console.log("mernstack is a programming language");
+//     break;
+//     case "c++":
+//     console.log("c++ is a programming language");
+//     break;
+//     case "c":
+//     console.log("c is a programming language");
+//     break;
+//     case "javascript":
+//     console.log("javascript is a programming language");
+//     break;
+//     default:
+//     console.log("not a programming language");
+// }
+//---------------------------------------------------------------------------------------
+// let age1 = 17;
+// if (age1 >= 18) {
+//     console.log("eligible for vote");
+// }
+// else {
+//     console.log("not eligible for vote");
+// }
+//-------------------------------------------------------------------------------------------------------------
+// let age = 28;
+ 
+//  let canVote= age>=18 ? "eligible for vote" : "not eligible for vote";
+// console.log(canVote);
+//-----------------------------------------------------------------------------------------------------------------
+// let age = 20;
+// let country = "india";
+
+// if (age >= 18 && country == "india") {
+//     console.log("eligible for vote");
+// }
+// else {
+//     console.log("not eligible for vote");
+// }
+//-----------------------------------------------------------------------------------------------------------------
+// let age=16;
+//  let country="usa";
+//   if(country ==="india")
+//   {
+//     if(age>=18)
+//     {
+//         console.log("eligible for vote");
+//     }
+//     else
+//     {
+//         console.log("not eligible for vote");
+//     }
+//   }
+//----------------------------------------------------------------------------------------------------------------
+ let i=10;
+do
+ {
+     console.log("Number: "+i);
+     i++;
+ } while(i<=20);
+//-----------------------------------------------------------------------------------------------------------------
+// for(let j=20; j>=10; j--)
+// {
+//     console.log("Number: "+j);
+// }
+//-----------------------------------------------------------------------------------------------------------------
+let students =[
+  {name:"arun", age:24, course:"mca"},//-------0
+  {name:"bala", age:23, course:"bca"},//-------1
+  {name:"charan", age:22, course:"bsc"},//-----2
+  {name:"charan", age:22, course:"bsc"},
+  {name:"dhanush", age:21, course:"bcom"}
+]
+console.log(students);
+console.log(students[2].name);
+console.log(students[1].name);
+console.log(students[0].name);
+
