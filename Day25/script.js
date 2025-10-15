679ed9a6ac8363963c4011857208f72261b06f3d
eeswari-s <eeswarivaithee@gmail.com>
2025-10-15 16:59:54 +0530

    c

---
+++ Day25/script.js
@@ -0,0 +1,65 @@
+// const names = ['Anil', 'Sunil', 'Kiran', 'Ravi'];
+
+//  names.forEach(function(name,index){
+//     console.log(index,name);
+//  });
+
+ 
+
+
+//-----------------------------------------------------------------------------------------------
+// const user ={
+//     name: 'Anil',
+//     age: 24,
+//     city: 'Bangalore'
+// };
+// for(let key in user){
+//     console.log(key, ":", user[key]);
+// }
+
+
+//-----------------------------------------------------------------------------------------------
+// const object ={
+//     name: 'Anil',
+//     age: 24,
+//     city: 'Bangalore'
+// };
+// console.log(object);
+//-----------------------------------------------------------------------------------------------
+// for (let i=1;i<=5;i++)
+//  {
+//     if(i===3){
+    
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//-----------------------------------------------------------------------------------------------
+//string
+// let name="Anil";
+// let message="welcome";
+//length
+// let text="mernstack";
+// console.log(text.length);
+// //------------------------------------------------------------------------------------------------------
+// //object
+// let person={
+//     name:"vaitheeswari",
+//     age:26,
+//     city:"chennai"
+// };
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.city);
+// console.log(person['name']);
+// console.log(person['age']);
+// console.log(person['city']);
+
+
+let user = {
+    fname: "Anil Kumar"
+};
+
+ console.log(user.fname); 
